cb9f6a4e4144fe13a0fb9e7726cff18cca34610f
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-29 23:02:52 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+# class Book
+#   attr_accessor :author, :page_count, :genre
+#   attr_reader :title
 
+#   def initialize(title)
+#     @title = title
+#   end
+
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+# end
+
+class Book 
+  attr_accessor :author, :page_count, :genre, :title
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
